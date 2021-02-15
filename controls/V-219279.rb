bf35316c5a3ff6e8203e2e084d264d07312e0cd7
yarick <yarick@yarick.net>
2021-02-15 17:03:15 -0500

    added describe

--- controls/V-219279.rb
+++ controls/V-219279.rb
@@ -64,5 +64,15 @@ required.
   tag fix_id: 'F-21003r305166_fix'
   tag cci: ['SV-109885', 'V-100781', 'CCI-000172']
   tag nist: ['AU-12 c']
-end
 
+  if os.arch == 'x86_64'
+    describe auditd.syscall('finit_module').where { arch == 'b64' } do
+      its('action.uniq') { should eq ['always'] }
+      its('list.uniq') { should eq ['exit'] }
+    end
+  end
+  describe auditd.syscall('finit_module').where { arch == 'b32' } do
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end
+end
