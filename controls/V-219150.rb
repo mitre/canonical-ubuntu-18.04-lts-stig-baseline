5bec44f2658df639211f8d74c15551b3b6ef98db
Greg Harrison <gharrison@gharriscBookPro.fios-router.home>
2021-03-14 18:35:34 -0400

    Both manual and automated check for crypttab and root partition

--- controls/V-219150.rb
+++ controls/V-219150.rb
@@ -69,5 +69,15 @@ difficult because the existing partitions must be resized and changed.
   tag fix_id: 'F-20874r304779_fix'
   tag cci: ['V-100525', 'SV-109629', 'CCI-001199', 'CCI-002475', 'CCI-002476']
   tag nist: ['SC-28', 'SC-28 (1)', 'SC-28 (1)']
-end
 
+  unless input('is_manual_nondefault_install_partition')
+    describe command('sudo lsblk | grep -A3 `cat /etc/crypttab |cut -d" "  -f1` |grep root | awk "{print $NF}"') do
+      its('exit_status') { should eq 0 }
+      its('stdout.strip') { should eq '/' }
+    end
+  else
+    describe 'Manual test' do
+      skip 'This control must be reviewed manually'
+    end
+  end
+end
