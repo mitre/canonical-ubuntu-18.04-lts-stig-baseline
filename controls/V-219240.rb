b4b618ae8f1c6ec318a355dca58771ee76f7f79d
Greg Harrison <gharrison@gharriscBookPro.fios-router.home>
2021-03-29 22:15:39 -0400

    Add rules and checks for V-219240 through V-219243

--- controls/V-219240.rb
+++ controls/V-219240.rb
@@ -56,5 +56,23 @@ auid!=4294967295 -k privileged-mount
   tag fix_id: 'F-20964r305049_fix'
   tag cci: ['V-100707', 'SV-109811', 'CCI-000172']
   tag nist: ['AU-12 c']
+
+  audit_file = '/bin/mount'
+
+  if file(audit_file).exist?
+    impact 0.5
+  else
+    impact 0.0
+  end
+
+  describe auditd.file(audit_file) do
+    its('permissions') { should include ['x'] }
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end file(audit_file).exist?
+
+  describe "The #{audit_file} file does not exist" do
+    skip "The #{audit_file} file does not exist, this requirement is Not Applicable."
+  end if !file(audit_file).exist?
 end
 
--- controls/V-219242.rb
+++ controls/V-219242.rb
@@ -56,5 +56,23 @@ auid!=4294967295 -k privileged-ssh
   tag fix_id: 'F-20966r305055_fix'
   tag cci: ['SV-109815', 'V-100711', 'CCI-000172']
   tag nist: ['AU-12 c']
+
+  audit_file = '/usr/bin/ssh-agent'
+
+  if file(audit_file).exist?
+    impact 0.5
+  else
+    impact 0.0
+  end
+
+  describe auditd.file(audit_file) do
+    its('permissions') { should include ['x'] }
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end file(audit_file).exist?
+
+  describe "The #{audit_file} file does not exist" do
+    skip "The #{audit_file} file does not exist, this requirement is Not Applicable."
+  end if !file(audit_file).exist?  
 end
 
