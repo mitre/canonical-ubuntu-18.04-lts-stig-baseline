61da8b717c33ad49ea0bbb5d2e45cc77fad73db6
Greg Harrison <gharrison@gharriscBookPro.fios-router.home>
2021-04-01 11:48:22 -0400

    First two audit_file rules V-219263 and V-219264 and checks

--- controls/V-219263.rb
+++ controls/V-219263.rb
@@ -56,5 +56,23 @@ auid!=4294967295 -k priv_cmd
   tag fix_id: 'F-20987r305118_fix'
   tag cci: ['SV-109855', 'V-100751', 'CCI-000172']
   tag nist: ['AU-12 c']
+
+  audit_file = '/usr/bin/sudo'
+
+  if file(audit_file).exist?
+    impact 0.5
+  else
+    impact 0.0
+  end
+
+  describe auditd.file(audit_file) do
+    its('permissions') { should include ['x'] }
+    its('action.uniq') { should eq ['always'] }
+    its('list.uniq') { should eq ['exit'] }
+  end file(audit_file).exist?
+
+  describe "The #{audit_file} file does not exist" do
+    skip "The #{audit_file} file does not exist, this requirement is Not Applicable."
+  end if !file(audit_file).exist?  
 end
 
