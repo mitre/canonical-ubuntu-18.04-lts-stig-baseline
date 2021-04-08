9e1942ec262ac1a8b95d5c056e673ce2c10b4ebc
Greg Harrison <greg.harrison@exptechinc.com>
2021-04-07 20:45:33 -0400

    Remove rules reload.  Not needed as a check.  Covered by ansble restart

--- controls/V-219225.rb
+++ controls/V-219225.rb
@@ -108,8 +108,5 @@ with the following command:
     it { should be_enabled }
     it { should be_running }
   end
-  describe command('augenrules --load') do
-    its('exit_status') { should eq 0 }
-  end
 end
 
